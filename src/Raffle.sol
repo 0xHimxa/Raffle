47288794a8c651f0484ea3438ed6972c10a9866a
himxa <himxa0x@gmail.com>
2025-11-27 02:06:49 +0100

    make it fuctional, remaing automation

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -1,5 +1,5 @@
 //SPDX-License-Identifier: MIT
-pragma solidity ^0.8.19;
+pragma solidity 0.8.30;
 
 //for anything u dont understand check chain link doc
 
@@ -12,18 +12,35 @@ import {VRFV2PlusClient} from "@chainlink/contracts/src/v0.8/vrf/dev/libraries/V
 //custom error
 
 error Raffile_SendMoreToEnterRaffile();
-
+error Raffile_TransferFaild();
+error Raffile__NotOPen();
 //contract lick the inherit contract and read through it
 contract Raffile is VRFConsumerBaseV2Plus {
+
+//type deleration
+
+//enum allow us to create basic custom type
+enum RaffileState {Open, //0
+
+ Calculating // 1
+  }
+
+
+   
+   
+   
+    //state variables
+    address private s_recentWinner;
     uint16 private constant REQUEST_CONFARMATION = 3;
     uint32 private constant NUM_WORDS = 1;
     uint256 private immutable I_enteranceFee;
     uint256 private immutable i_interval;
-    uint256 private immutable i_lastTimeStamp;
+    uint256 private  s_lastTimeStamp;
     bytes32 private immutable i_keyHash;
     uint256 private immutable i_subscriptionId;
     uint32 private immutable i_callbackGasLimit;
     address[] public s_players;
+    RaffileState private s_raffleState;
 
     /*Event*/
 
@@ -31,6 +48,7 @@ contract Raffile is VRFConsumerBaseV2Plus {
     //the fronted listen to it
     //the reason we dont use storage var is because they are expensive
     event RaffileEntered(address indexed player);
+    event WinerPicked(address indexed winner)
 
     // the contract we inherit from have contructor that accept so we need to pass it in
     //like this
@@ -45,7 +63,7 @@ contract Raffile is VRFConsumerBaseV2Plus {
     ) VRFConsumerBaseV2Plus(vrfCodinator) {
         I_enteranceFee = enteranceFee;
         i_interval = _interval;
-        i_lastTimeStamp = block.timestamp;
+        s_lastTimeStamp = block.timestamp;
         i_keyHash = gaslane;
         i_subscriptionId = subid;
         i_callbackGasLimit = callbackgaslim;
@@ -58,6 +76,10 @@ contract Raffile is VRFConsumerBaseV2Plus {
         if (msg.value < I_enteranceFee) {
             revert Raffile_SendMoreToEnterRaffile();
         }
+
+        if(s_raffleState !=  RaffileState.Open){
+            revert Raffile__NotOPen();
+        }
         s_players.push(payable(msg.sender));
         emit RaffileEntered(msg.sender);
     }
@@ -65,10 +87,19 @@ contract Raffile is VRFConsumerBaseV2Plus {
     //get a random number
     //use random number to pick a winner
 
+
+
+
+
+
+
     function pickWinner() external {
-        if ((block.timestamp - i_lastTimeStamp) < i_interval) {
+
+       
+        if ((block.timestamp - s_lastTimeStamp) < i_interval) {
             revert();
         }
+        s_raffleState = RaffileState.Calculating;
 
         // this below is gotton from a library we imported
 
@@ -91,10 +122,47 @@ contract Raffile is VRFConsumerBaseV2Plus {
           uint256 requestId = s_vrfCoordinator.requestRandomWords(request);
     }
 
-    function fulfillRandomWords(
+
+
+/**
+ * wen writing a fn always keep CEI: Chekcs, effect,interaction
+ * checks eg require if at the top
+ * then effect(internal contract state)
+ * interaction (eternal contrac interaction )
+ * like we write in below code
+ */
+
+
+function fulfillRandomWords(
         uint256 requestId,
         uint256[] calldata randomWords
-    ) internal override {}
+    ) internal override {
+
+
+// the oracale return array of numbers eg this long 6795465894930204588588493992939393993229
+
+//so we % by eg 10 then we use the remender to pick a random winner
+
+/** effect internal contract state */
+uint256 indexOfWinner = randomWords[0] % s_players.length;
+address payable recentWinner = s_players[indexOfWinner];
+s_recentWinner = recentWinner;
+emit WinerPicked(recentWinner);
+
+
+/**Interaction (EXternal contract Interaction) */
+
+(bool success,) = recentWinner.call{value:address(this).balance}("");
+
+if(!success){
+    revert Raffile_TransferFaild();
+}
+
+s_raffleState = RaffileState.Open;
+s_players = new address[](0);
+s_lastTimeStamp = block.timestamp;
+
+    }
 
     function getEnteranceFee() public view returns (uint256) {
         return I_enteranceFee;
